7eeaef77f6c71a5b75c59f2b2891136bc2a45d88
KolodziejczykTomasz <tomaszkolodziejczyk@wp.pl>
2022-10-13 16:13:29 +0200

    Added

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <script>
-
+import clickOutSide from "@mahdikhashan/vue3-click-outside";
 export default {
+  directives: {
+    clickOutSide,
+  },
   data: () => ({
     isPhoneShow: false,
     isEmailShow: false,
@@ -8,47 +11,49 @@ export default {
       display: "block"
     },
   }),
-  components: {},
   methods: {
     closingPhone() {
-      this.isEmailShow = false,
+      this.isEmailShow = false
         this.isPhoneShow = !this.isPhoneShow
     },
     closingEmail() {
-      this.isPhoneShow = false,
+      this.isPhoneShow = false
         this.isEmailShow = !this.isEmailShow
     },
-    closeMouseMove() {
-      this.isPhoneShow = false,
-        this.isEmailShow = false
+    onClickOutside () {
+      this.isPhoneShow = false
+      this.isEmailShow = false
     }
-  }
+  },
 }
-import ClickOutside from 'vue-click-outside'
+
+
 </script>
 <template>
-  <div class="wrapper">
+  <div class="wrapper" v-cloak>
     <div class="column">
       <div class="firstRow">
         NETTOM
         <span class="greyText">24</span>
       </div>
-      <div class="secondRow">
-        <div class="icon-wrapper">
-        <font-awesome-icon icon="fa-solid fa-square-phone" @click="closingPhone()" ClickOutside="closeMouseMove()"/>
+      <div class="secondRow" v-click-out-side="onClickOutside" >
+        <div class="icon-wrapper hoverGrayColor">
+          <font-awesome-icon icon="fa-solid fa-square-phone"
+           @click="closingPhone()" class="iconSocial"/>
         </div>
-        <div class="icon-wrapper">
-         <font-awesome-icon icon="fa-solid fa-square-envelope" @click="closingEmail()" ClickOutside="closeMouseMove()"/>
+        <div class="icon-wrapper hoverGrayColor">
+           <font-awesome-icon icon="fa-solid fa-square-envelope"
+           @click="closingEmail()" class="iconSocial"/>
         </div>
         <div class="icon-wrapper">
           <a href="https://www.facebook.com/KolodziejczykTomasz44" target="_blank" rel="noopener noreferrer"
             tabIndex="-3">
-            <font-awesome-icon icon="fa-brands fa-square-facebook" />
+            <font-awesome-icon icon="fa-brands fa-square-facebook"  class="iconSocial"/>
           </a>
         </div>
         <div class="icon-wrapper">
           <a href="https://github.com/KolodziejczykTomasz" target="_blank" rel="noopener noreferrer" tabIndex="-4">
-            <font-awesome-icon icon="fa-brands fa-square-github" />
+            <font-awesome-icon icon="fa-brands fa-square-github" class="iconSocial"/>
           </a>
         </div>
       </div>
@@ -56,10 +61,12 @@ import ClickOutside from 'vue-click-outside'
   </div>
   <div class="dashboard">
     <div class="content">   
-      <div class="wrapperPhoneNumber" :style="isPhoneShow ? hoverStyle : null">
+      <div class="wrapperPhoneNumber"
+           :style="isPhoneShow ? hoverStyle : null" >
         <a href="tel: +48.512893100"> +48.512893100</a>
       </div>     
-      <div class="wrapperEmail" :style="isEmailShow ? hoverStyle : null">
+      <div class="wrapperEmail" :style="isEmailShow ? hoverStyle : null"
+          >
         <a href="mailto:tomaszkolodziejczyk@wp.pl" target="_blank" rel="noopener noreferrer">
           tomaszkolodziejczyk@wp.pl
         </a>
